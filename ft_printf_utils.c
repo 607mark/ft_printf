874a5bcc24aead822edd31727fa5ba085ee1a9d2
mshabano <40577872+607mark@users.noreply.github.com>
2024-05-30 22:18:26 +0300

    added utils

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,9 +6,11 @@
 /*   By: mshabano <mshabano@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 19:49:26 by mshabano          #+#    #+#             */
-/*   Updated: 2024/05/30 20:01:11 by mshabano         ###   ########.fr       */
+/*   Updated: 2024/05/30 21:41:13 by mshabano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -33,7 +35,20 @@ int	ft_toupper(int c)
 	return (c);
 }
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
 int putstr(char *s, int len)
 {
+	if (len == -1)
+		len = ft_strlen(s);
 	write (1, s, len);
+	return (len);
 }
